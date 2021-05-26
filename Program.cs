52edf34e2d545449d4599d9c150affbf9b8e7d0f
Erica Slotkin <77819164+ferris4936@users.noreply.github.com>
2021-05-26 15:05:45 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;  
+
+namespace Slotkin_P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.TestCelsiusToKelvin();
+        }
+
+        static double CelsiusToKelvin(double celsius)
+        {
+            double kelvin = celsius + 273.15;
+            return kelvin;   
+        }
+
+        static void TestCelsiusToKelvin()
+        {
+            double kelvin = Program.CelsiusToKelvin(0.0);
+            Console.WriteLine("CelsiusToKelvin(0.0) => {0}", kelvin);
+        }
+    }
+}
+;
